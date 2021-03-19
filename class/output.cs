cb1d9fb18a2901f6965588f46e179f94a23171f1
SmaugthurLaptop <danirayoroldan@gmail.com>
2021-03-19 07:06:56 -0600

    correct string output formatting to show only 3 decimals on the relative frecuencies

--- class/output.cs
+++ class/output.cs
@@ -14,7 +14,7 @@ namespace OUTPUT
             Console.WriteLine($"\n\n|{"VALUE",-15}|{"FRECUENCY",-15}|{"CUMULUTATIVE F",-15}|{"RELATIVE F", -15}|");
             for(int i=0; i < Data.frecuencyValues.Length; i++)
             {
-                Console.WriteLine($"|{Data.frecuencyValues[i],-15}|{Data.absoluteFrecuency[i],-15}|{Data.cumulativeFrecuency[i],-15}|{Data.relativeFrecuency[i], +15}|");
+                Console.WriteLine($"|{Data.frecuencyValues[i],-15}|{Data.absoluteFrecuency[i],-15}|{Data.cumulativeFrecuency[i],-15}|{Data.relativeFrecuency[i], +15:F3}|");
             }
             Console.WriteLine($"|{"TOTAL",15}|{Data.inputData.Length,15}|{"",15}|\n");
         }
@@ -27,7 +27,7 @@ namespace OUTPUT
             Console.WriteLine($"\n\n|{"VALUE",-15}|{"FRECUENCY",-15}|{"CUMULUTATIVE F",-15}|{"RELATIVE F", -15}|");
             for(int i=0; i < Data.frecuencyValues.GetLength(0); i++)
             {
-                Console.WriteLine($"|{Data.frecuencyValues[i,0] + " - " + Data.frecuencyValues[i,1],-15}|{Data.absoluteFrecuency[i],-15}|{Data.cumulativeFrecuency[i],-15}|{Data.relativeFrecuency[i], +15}|");
+                Console.WriteLine($"|{Data.frecuencyValues[i,0] + " - " + Data.frecuencyValues[i,1],-15}|{Data.absoluteFrecuency[i],-15}|{Data.cumulativeFrecuency[i],-15}|{Data.relativeFrecuency[i], +15:F3}|");
             }
             Console.WriteLine($"|{"TOTAL",15}|{Data.inputData.Length,15}|{"",15}|\n");
         }
